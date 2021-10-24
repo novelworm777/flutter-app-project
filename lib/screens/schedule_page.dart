98e8b9134db301d5ef992061dee45e04049e4ec7
Mikhaya J. R. Hutagalung <9736217-novelworm777@users.noreply.gitlab.com>
2021-10-24 22:18:26 +0700

    update schedule page UI

--- lib/screens/schedule_page.dart
+++ lib/screens/schedule_page.dart
@@ -23,30 +23,36 @@ class _SchedulePageState extends State<SchedulePage> {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Expanded(
-              flex: 1,
+              flex: 2,
               child: Row(
                 children: [
                   Expanded(child: Container()),
                   Expanded(
                     flex: 5,
-                    child: Text(
-                      widget.weekday,
-                      textAlign: TextAlign.center,
-                      style: kWeekdayTitleStyle,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          widget.weekday,
+                          textAlign: TextAlign.center,
+                          style: kWeekdayTitleStyle,
+                        ),
+                        const SizedBox(height: 7.0),
+                        const SizedBox(
+                          height: 10.0,
+                          width: 210.0,
+                          child: Divider(color: Colors.white),
+                        ),
+                      ],
                     ),
                   ),
                   const AppSettings(),
                 ],
               ),
             ),
-            const SizedBox(
-              height: 10.0,
-              width: 210.0,
-              child: Divider(color: Colors.white),
-            ),
             const SizedBox(height: 21.0),
             Expanded(
-              flex: 7,
+              flex: 10,
               child: widget._scheduleBrain.getEvents(),
             ),
             const SizedBox(height: 37.0)
